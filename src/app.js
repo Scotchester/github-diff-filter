39d661feeebdbd7377453be2b2386ed99085ccf0
Scott Cranfill <scott.cranfill@cfpb.gov>
2014-07-10 10:51:57 -0400

    Add button to show previously-hidden files

--- src/app.js
+++ src/app.js
@@ -1,29 +1,34 @@
 (function () {
 
-  console.log('Initializing github-diff-filter');
+  // console.log('Initializing github-diff-filter');
 
   // Includes processed by grunt-include-replace.
   var css = '<style>@@include("style.min.css")</style>';
   var form = '@@include("../temp/html/form.html")';
 
-  // Attach the CSS to the head and the overlay markup to the body.
-  $('head').append(css);
-  $('.header .container').append(form);
+  // If not already done, attach the CSS to the head and the overlay markup to the body.
+  if ($('#gdf').length == 0) {
+    $('head').append(css);
+    $('.header .container').append(form);
+  }
 
-  // Create the callback function to bind to the event listeners below.
+  // Create the function that hides files.
   window.gdfHide = function (event) {
     // If hitting enter (from the input) or clicking the button.
     if (event.which == 13 || event.which == 1) {
       var query = $('#gdf-hide-input').val();
 
       if (query !== '') {
-        console.log("github-diff-filter: hiding paths matching '" + query + "'");
+        // console.log("github-diff-filter: hiding paths matching '" + query + "'");
         
         // Remove from Diff Stats list.
         $('a[href^="#diff"]:contains(' + query + ')').parent().hide();
 
         // Remove the actual file diffs.
         $('[data-path*="' + query + '"]').parent().hide();
+
+        // Enable the "Show all files" button
+        $('#gdf-show-all-btn').removeAttr('disabled');
 
         // Clear the input field and refocus on it for entering another value.
         $('#gdf-hide-input').val('').focus();
@@ -32,9 +37,26 @@
     }
   };
 
-  // Bind event listeners to the button and input field that call gdfHide
+  // Bind event listeners to the button and input field that call gdfHide.
   $('#gdf-hide-btn').click(gdfHide);
   $('#gdf-hide-input').keyup(gdfHide);
+
+  // Create the function that shows files.
+  window.gdfShow = function (event) {
+    // console.log("github-diff-filter: showing all files");
+      
+    // Show all in Diff Stats list.
+    $('a[href^="#diff"]').parent().show();
+
+    // Show all of the actual file diffs.
+    $('[data-path]').parent().show();
+
+    // Hide the "Show all files" button.
+    $('#gdf-show-all-btn').attr('disabled', true);
+  };
+
+  // Bind event listener to the "Show all files" button.
+  $('#gdf-show-all-btn').click(gdfShow);
 
   // Hide the overlay if the close button is clicked.
   $('#gdf-close-btn').click(function () {
